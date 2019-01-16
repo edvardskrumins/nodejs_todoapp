69a2fab2681e8a45a46d464ed179b2b2d9ae54ef
edvardskrumins <edvardskrumins@gmail.com>
2019-01-16 12:06:10 +0200

    Added post json data evaluation using Postman

---
+++ server/models/todo.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    enum: ['beans', 'nuts'],
+    trim: true, // removes whitespace
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    min: [0000, 'Not a valid time'],
+    max: 2400,
+    default: null
+
+  }
+});
+
+module.exports = {
+  Todo
+};

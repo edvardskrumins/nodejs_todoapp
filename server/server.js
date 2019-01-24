bb7feb33c048657f4ab87bf465305ddd9d99e6d8
edvardskrumins <edvardskrumins@gmail.com>
2019-01-24 19:11:21 +0200

    Added todos delete function

--- server/server.js
+++ server/server.js
@@ -47,7 +47,20 @@ app.get('/todos/:id', (req,res) => {
     res.status(400).send(e);
   });
 });
-
+app.get('/todosDelete/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(401).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+  if(!todo) {
+    return res.status(404).send();
+    }
+  res.send('Collection deleted');
+    }).catch((err) => {
+  res.status(400).send(err);
+  });
+});
 
 
 
